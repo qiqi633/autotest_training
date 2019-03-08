d4d27302f55413075cab8c3ba6db9b43ca58892d
qiqi633 <630345353@qq.com>
2019-03-08 20:11:54 +0800

    修改test.py

--- scripts_test/test.py
+++ scripts_test/test.py
@@ -7,6 +7,7 @@ sys.path.append("C:\Python27\Lib\site-packages")
 # importlib.reload(sys)
 import time
 import pytest
-a= 1
-b = 2
-print('a:{0},b:{1},c:{2}'.format(a,b,a+b))
+def test():
+    a= 1
+    b = 2
+    print('a:{0},b:{1},c:{2}'.format(a,b,a+b))
